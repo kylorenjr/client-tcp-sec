dcc4e750be1e53354a49c9a6b583292c96eee450
Lucas M Ventura <lmventura311205@gmail.com>
2025-08-21 19:20:43 -0300

    Adicionado lógica do servidor e mensagem com o client

--- server.py
+++ server.py
@@ -1,2 +1,19 @@
 #servidor em python
 import socket
+import sys
+
+porta = int(sys.argv[1]) #porta passada como argumento, "arg 1"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #se deixar em branco tem o mesmo efeito! add ipv4 e tcp
+s.bind(("0.0.0.0", porta))
+s.listen(1)
+print("Aguardando conexão na porta {}".format(porta))
+
+s2, endereco = s.accept()
+print("Conectado com {}".format(endereco))
+
+while True:
+    msg = s2.recv(1024).decode()
+    print(msg)
+
+    msg2 = input() + "\n"
+    s2.send(msg2.encode())
