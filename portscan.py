055d5cb4a2e8327427f865934ba50942557e87a3
Lucas M Ventura <lmventura311205@gmail.com>
2025-08-21 20:48:33 -0300

    adicionando um banner grabbing

--- portscan.py
+++ portscan.py
@@ -3,6 +3,7 @@ import socket
 import sys
 
 portas = [21, 22, 23, 25, 53, 80, 110, 143, 443, 3306]  #portas comuns que serão verificadas
+host = sys.argv[1]  #host passado como argumento, "arg 1"
 
 for porta in portas:
     meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,7 +12,17 @@ for porta in portas:
     code  = meusocket.connect_ex((sys.argv[1], porta))
 
     if code == 0:
-        print("Porta {} aberta".format(porta)) 
+        meusocket.connect((host, porta))
+
+        try:
+            banner = meusocket.recv(1024).decode().replace("\n", "")
+        except:
+            banner = "Nenhum banner identificado"
+
+        print(porta, banner)
+
+        meusocket.close()
+
 
 #implementar o banner grabbing
 ''' 
